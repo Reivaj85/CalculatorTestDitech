aa5b23ef3cfb8914ff53c4c2447909d69de9abd6
Reivaj85 <ing.oscarjavics@gmail.com>
2019-04-05 15:40:31 -0500

    Base Proyecto
    Se realiza la solucion del proyecto sobre el cual se va realizar la logica respectiva.

---
+++ DitechCalculator/Calculator.ConsoleClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Calculator.ConsoleClient {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
